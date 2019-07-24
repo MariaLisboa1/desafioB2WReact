aff6005b9160178a1b9d76503f30511b88d39d7d
zaza.naza95 <zaza.naza95@gmail.com>
2019-07-24 11:59:45 -0300

    at

--- src/page/home.js
+++ src/page/home.js
@@ -7,57 +7,110 @@ class App extends Component {
 
     state = {
         planet: [],
-        films: []
+        films: [],
+        notFilm: ''
     }
 
     numFilm = [];
     films = [];
+    btn
+    span
 
     async componentDidMount() {
-        const response = await api.get(`planets/1/`);
-        this.setState({ planet: response.data });
+        this.getPlanet();
+    }
 
-        if (this.state.planet.films[0]) {
-            this.state.planet.films.forEach(e => {
-                const film = e;
-                const numFilm = film.charAt(film.length - 2);
-                this.numFilm.push({
-                    numFilms: numFilm
+    async getPlanet() {
+        await this.setState({ planet: [] });
+        this.getBtn = document.getElementById('btn');
+        this.span = document.getElementById('span');
+
+        try {
+
+            const response = await api.get(`planets/${this.numRandom()}/`);
+            await this.setState({ planet: response.data });
+
+            if (this.state.planet.films[0]) {
+                await this.setState({ notFilm: "" });
+
+                this.state.planet.films.forEach(e => {
+                    const film = e;
+                    const numFilm = film.charAt(film.length - 2);
+                    this.numFilm.push({
+                        numFilms: numFilm
+                    });
                 });
-               
-            });
-            this.getFilms(this.numFilm);
-            
+                this.getFilms(this.numFilm);
+            } else {
+                this.setState({ notFilm: " No film." });
+                this.getBtn.classList.add("btn");
+                this.getBtn.classList.remove("is-loading");
+                await this.setState({ films: [] });
+            }
+
+        } catch (error) {
+            this.handleError(error);
         }
 
-        
     }
 
     async getFilms(numFilm) {
-        
-        await numFilm.forEach( async (e) =>  {
-            const response = await api.get(`films/${e.numFilms}/`);
-            this.films.push({
-                films: response.data.title
+
+        try {
+            await this.setState({ films: [] });
+
+            await numFilm.forEach(async (e) => {
+                const response = await api.get(`films/${e.numFilms}/`);
+                setTimeout(() => {
+                    for (var i = 0; i < this.films.length; i++) {
+                        delete this.films[i];
+                        delete this.state.films[i]
+                    }
+                }, 1000);
+
+                setTimeout(() => {
+                    this.films.push({
+                        films: response.data
+                    });
+                    this.setState({ films: this.films });
+                }, 2000);
             });
-        });
-
-
-       
-        
-        await 
-
-        setTimeout(() => {
-            this.setState({films: this.films});
-        }, 2000);
-        
+            this.getBtn.classList.add("btn");
+            this.getBtn.classList.remove("is-loading");
+        } catch (error) {
+            this.handleError(error);
+        }
     }
 
     async handleReload() {
-        this.state.planet = [];
-        const response = await api.get(`planets/3/`);
-        this.setState({ planet: response.data });
-        
+        this.getBtn = document.getElementById('btn');
+        this.getBtn.classList.add("is-loading");
+
+        this.getPlanet();
+    }
+
+    numRandom() {
+
+        let drawn = [];
+        const maxValue = 61;
+
+        if (drawn.length === maxValue) {
+            if (alert('No more planets :(')) drawn = [];
+            else return;
+        }
+        let suggestion = Math.ceil(Math.random() * maxValue);
+        while (drawn.indexOf(suggestion) >= 0) {
+            suggestion = Math.ceil(Math.random() * maxValue);
+        }
+        drawn.push(suggestion);
+        return suggestion;
+    }
+
+    handleError(error) {
+        this.getBtn.classList.add("is-fail");
+        this.span.innerHTML = "ERROR";
+        alert("An error has occurred. Please try again later");
+        console.log(`ERROR => ${error}`);
     }
 
     render() {
@@ -76,18 +129,20 @@ class App extends Component {
                     </div>
 
                     <div className="nfilms">
-                    
-                    {this.state.films.map(filmsName => (
-                        <p key={filmsName.films}>
-                           {filmsName.films}
-                        </p>
-                    ))}
-                    
+                        FEATURED IN:
+                        {this.state.notFilm}
+                        {this.state.films.map(filmsName => (
+                            // <p key={this.state.films[0].films.created}>
+                            <p key={filmsName.films.created}>
+                                {filmsName.films.title}
+                            </p>
+                        ))}
                     </div>
-              
-                </div>
 
-                <button type="button" onClick={() => this.handleReload()}>NEXT</button>
+                </div>
+                <button type="button" className="btn" id="btn" onClick={() => this.handleReload()}>
+                    <span id="span">NEXT</span>
+                </button>
             </div>
 
         );
